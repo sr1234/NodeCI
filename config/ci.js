391859ec831823cb546f2044adec7ef10caa6625
Joey Snickles <noneof@yourbeeswax.com>
2019-08-08 21:47:38 -0700

    added the ci config

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '229734467707-mtgd9rq6mi2k21d5tp6cbm9ngtfl69s2.apps.googleusercontent.com',
+  googleClientSecret: 'vch5d83ngmcyFZB1qQFdPtcq',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
